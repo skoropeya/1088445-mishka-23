8daeb407951788708f4fe76357b910d24c1bd685
Kate <skoropeya@inbox.ru>
2021-08-06 17:37:59 +0300

    Исправления

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,10 @@
+const mainNav = document.querySelector(".main-nav");
 const mainMenu = document.querySelector(".main-nav__list");
 const toggle = document.querySelector(".main-nav__toggle");
+
+if (mainNav.classList.contains("main-nav--no-js")) {
+  mainNav.classList.remove('main-nav--no-js');
+}
 
 toggle.addEventListener("click", function(evt) {
   evt.preventDefault();
