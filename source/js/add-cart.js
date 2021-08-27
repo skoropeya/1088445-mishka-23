65f66b1a9c8fd20ab9f3197887837ee25710129f
Kate <skoropeya@inbox.ru>
2021-08-27 16:14:44 +0300

    добавлен скрипт для открытия модального окна на странице каталога

--- source/js/add-cart.js
+++ source/js/add-cart.js
@@ -1,14 +1,15 @@
-const makeOrder = document.querySelector(".product__make-order");
+const addInCart = document.querySelectorAll(".card__add-cart");
 const modalFormSize = document.querySelector(".modal");
 const btnInModal = document.querySelector(".form-size__add");
 const overlay = document.querySelector(".page-body__overlay");
 
-//добавить товар в корзину (кнопка "Заказать" на главной)
-makeOrder.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalFormSize.classList.add("modal--opened");
-  overlay.classList.add("page-body__overlay--opened");
-});
+//добавить товар в корзину (кнопка с иконкой в карточке товара)
+for (let i=0; i < addInCart.length; i++) {
+  addInCart[i].addEventListener("click", function() {
+    modalFormSize.classList.add("modal--opened");
+    overlay.classList.add("page-body__overlay--opened");
+  });
+}
 
 //закрыть модальное окно кнопкой "Добавить"
 btnInModal.addEventListener("click", function(evt) {
